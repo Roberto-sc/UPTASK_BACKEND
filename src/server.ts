e264a9114ad269971979dbeaf80623881f327c48
Roberto-sc <luis.solano@ucrso.info>
2024-11-23 14:15:25 -0600

    23/11

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from 'dotenv'
 import { connectDB } from "./config/db";
 import projectRoutes from './routes/projectRoutes'
+import cors from 'cors'
+import { corsConfig } from "./config/cors";
 
 
 dotenv.config()
@@ -10,6 +12,7 @@ dotenv.config()
 connectDB()
 const app = express()
 
+app.use(cors(corsConfig))
 app.use(express.json())
 
 //Routes
